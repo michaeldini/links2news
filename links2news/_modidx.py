2b663e353289496846ff7e0d45539c3a5ad25033
michaeldini <michael.dini@gmail.com>
2022-11-20 00:05:23 -0500

    Nothing new, just syncing

--- links2news/_modidx.py
+++ links2news/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://michaeldini.github.io',
                 'git_url': 'https://github.com/michaeldini/links2news',
                 'lib_path': 'links2news'},
-  'syms': {'links2news.links2news': {'links2news.links2news.get_soup': ('nature.html#get_soup', 'links2news/links2news.py')}}}
+  'syms': { 'links2news.mittech': {'links2news.mittech.get_soup': ('mittech.html#get_soup', 'links2news/mittech.py')},
+            'links2news.nature': {'links2news.nature.get_soup': ('nature.html#get_soup', 'links2news/nature.py')}}}
